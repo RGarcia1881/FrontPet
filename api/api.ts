e28e88d48dbc48fc55d5ca903f0161cebd811cd3
RadulfMenendo <r.menendez011010@gmail.com>
2025-09-04 19:29:03 -0600

    Update Notes and api

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Reemplaza con la IP o dominio de tu backend Django
-const BASE_URL = 'http://127.0.0.1:8000/api/v1/';
+const BASE_URL = 'http://192.168.0.231:8000/api/v1/';
 
 const API = axios.create({
   baseURL: BASE_URL,
